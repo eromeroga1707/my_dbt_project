52b9a63584980a9db3af065b6fcf446aa5125095
eliromgar07@gmail.com <eliromgar07@gmail.com>
2025-04-15 14:38:49 +0000

    feat: mrt_order_monthly_report

--- models/mart/mrt_order_monthly_report.sql
+++ models/mart/mrt_order_monthly_report.sql
@@ -1,37 +1,29 @@
-WITH monthly_users_recap AS (
+WITH monthly_recap AS (
+    SELECT 
+        DATE_TRUNC('month', order_created_at) AS order_month,
+        COUNT(DISTINCT user_id) AS total_monthly_users,
+        COUNT(order_id) AS total_monthly_orders
+    FROM {{ ref('int_sales_database__order') }}
+    GROUP BY order_month
+),
 
-
-SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(DISTINCT user_name) AS total_monthly_users
-FROM {{ ref('stg_sales_database__order')}}
-GROUP BY order_month
-ORDER BY total_monthly_users DESC
-
-
-), total_monthly_user_from_jawa_timur AS (
-SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(DISTINCT user.user_name) AS total_monthly_users_from_jawa_timur
-FROM {{ ref('stg_sales_database__order')}} AS orders
-LEFT JOIN {{ ref('stg_sales_database__user')}} AS user ON user.user_name = orders.user_name
-WHERE user.customer_state LIKE '%JAWA%TIMUR%'
-GROUP BY order_month
-
-
-), monthly_orders_recap AS (
-
-
-SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(order_id) AS total_monthly_orders
-FROM {{ source('sales_database', 'order')}}
-GROUP BY order_month
-
-
+total_monthly_user_from_jawa_timur AS (
+    SELECT 
+        DATE_TRUNC('month', order_created_at) AS order_month,
+        COUNT(DISTINCT o.user_id) AS total_monthly_users_from_jawa_timur
+    FROM {{ ref('int_sales_database__order') }} AS o
+    LEFT JOIN {{ ref('int_sales_database__user_favorite_product') }} AS u 
+        ON o.user_id = u.user_id
+    WHERE o.user_state ILIKE '%JAWA%TIMUR%'
+    GROUP BY order_month
 )
-SELECT u.order_month,
-COALESCE(u.total_monthly_users,0) AS total_monthly_users,
-COALESCE(jt.total_monthly_users_from_jawa_timur,0) AS total_monthly_users_from_jawa_timur,
-COALESCE(o.total_monthly_orders,0) AS otal_monthly_orders
-FROM monthly_users_recap AS u
-LEFT JOIN total_monthly_user_from_jawa_timur AS jt ON jt.order_month = u.order_month
-LEFT JOIN monthly_orders_recap AS o ON o.order_month = u.order_month
-ORDER BY order_month
+
+SELECT 
+    r.order_month,
+    COALESCE(r.total_monthly_users, 0) AS total_monthly_users,
+    COALESCE(jt.total_monthly_users_from_jawa_timur, 0) AS total_monthly_users_from_jawa_timur,
+    COALESCE(r.total_monthly_orders, 0) AS total_monthly_orders
+FROM monthly_recap AS r
+LEFT JOIN total_monthly_user_from_jawa_timur AS jt 
+    ON jt.order_month = r.order_month
+ORDER BY r.order_month
