30d721999e5f98edc08cced2cd889f9a6614b4f4
eliromgar07@gmail.com <eliromgar07@gmail.com>
2025-04-15 19:13:10 +0000

    update

--- models/mart/mrt_reviews_availability.sql
+++ models/mart/mrt_reviews_availability.sql
@@ -4,6 +4,7 @@ materialized='table',
 tag='hourly'
 )
 }}
+
 with listings as (
 select
 neighbourhood_cleansed,
@@ -12,13 +13,15 @@ number_of_reviews,
 availability_365
 from {{ ref('stg_airbnb__listings') }}
 ),
+
 reviews_availability as (
 select
 neighbourhood_cleansed,
 room_type,
 sum(number_of_reviews) as total_reviews,
-round(avg(availability_365,2 as avg_availability
+round(avg(availability_365,2) as avg_availability
 from listings
 group by 1, 2
 )
+
 select * from reviews_availability
